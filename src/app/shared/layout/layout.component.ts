585191ba45a7d403f28f1ce33bf4ba2510576331
ninad99 <ninuch7@gmail.com>
2020-06-14 23:55:42 +0530

    Updated layout

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from '../../user/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../models/appstate.model';
 
 @Component({
   selector: 'app-layout',
@@ -11,17 +13,23 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class LayoutComponent implements OnInit {
   isHandset$: Observable<boolean>;
+  isLoggedIn$: Observable<boolean>;
 
-  constructor(private breakpointObserver: BreakpointObserver, private afAuth: AngularFireAuth) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
     this.isHandset$ = this.breakpointObserver.observe([Breakpoints.Handset]).pipe(
       map(result => result.matches),
       shareReplay()
     );
+    this.isLoggedIn$ = this.store.select(state => state.auth.isLoggedIn);
   }
 
   logout(): void {
-    this.afAuth.signOut();
+    this.authService.signOut();
   }
 }
