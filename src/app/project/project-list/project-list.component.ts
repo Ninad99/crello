0f51fccf704b655fe9dfaac3d4464f23be479360
ninad99 <ninuch7@gmail.com>
2020-06-15 23:08:10 +0530

    Project list component created

---
+++ src/app/project/project-list/project-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription, Observable } from 'rxjs';
+import { ProjectService } from '../project.service';
+import { SnackService } from '../../services/snack.service';
+import { CreateProjectDialogComponent } from '../dialog-components/create-project-dialog.component';
+import { Project } from '../../models/project.model';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.scss']
+})
+export class ProjectListComponent implements OnInit, OnDestroy {
+  _subscription: Subscription;
+  projects$: Observable<Project[]>;
+
+  constructor(
+    private projectService: ProjectService,
+    private dialog: MatDialog,
+    private snackService: SnackService
+  ) {}
+
+  ngOnInit(): void {
+    this.projects$ = this.projectService.getUserProjects();
+  }
+
+  ngOnDestroy(): void {
+    if (this._subscription) this._subscription.unsubscribe();
+  }
+
+  openCreateProjectDialog(): void {
+    const dialogRef = this.dialog.open(CreateProjectDialogComponent, {
+      width: '400px'
+    });
+
+    this._subscription = dialogRef.afterClosed().subscribe(async (result: { created: boolean }) => {
+      if (result?.created) {
+        this.snackService.openWithMessage('Successfully created!', 'Dismiss');
+        this._subscription.unsubscribe();
+      }
+    });
+  }
+}
