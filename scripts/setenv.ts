92a429bdcd1df505345caf5cbf86e95b2b2a2886
ninad99 <ninuch7@gmail.com>
2020-06-17 23:41:57 +0530

    setenv script

---
+++ scripts/setenv.ts
@@ -0,0 +1,41 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+
+// read environment variables from .env file
+require('dotenv').config();
+
+// read the command line argument passed
+// with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
+
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+  production: ${isProduction},
+  firebase: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
+  }
+};
+`;
+
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Wrote variables to ${targetPath}`);
+});
