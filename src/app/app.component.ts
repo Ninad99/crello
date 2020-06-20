5ab1f032d73b308d164ab04c681b5c44a9c6ef62
ninad99 <ninuch7@gmail.com>
2020-06-20 23:05:44 +0530

    Added redirect to url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Store } from '@ngrx/store';
 import { AppState } from './models/appstate.model';
 import { LoginSuccess } from './store/actions/auth.actions';
+import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loading: boolean;
+  _redirectSubscription: Subscription;
+  redirectUrl: string;
 
-  constructor(private afAuth: AngularFireAuth, private store: Store<AppState>) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private store: Store<AppState>,
+    private router: Router
+  ) {
     this.loading = true;
   }
 
   ngOnInit(): void {
+    this._redirectSubscription = this.store
+      .select(state => state.auth.redirectUrl)
+      .subscribe((url: string) => {
+        if (url) {
+          this.redirectUrl = url;
+        }
+      });
     this.afAuth.onAuthStateChanged((user: firebase.User) => {
       if (user) {
         this.store.dispatch(new LoginSuccess());
+        if (this.redirectUrl) this.router.navigate([this.redirectUrl]);
         this.loading = false;
       } else {
         this.loading = false;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this._redirectSubscription) this._redirectSubscription.unsubscribe();
   }
 }
