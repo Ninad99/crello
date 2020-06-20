676a8bbc6646700cfdad3112fe9774e070217cfe
ninad99 <ninuch7@gmail.com>
2020-06-20 23:03:05 +0530

    Storing redirect url to store

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { SnackService } from 'src/app/services/snack.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../models/appstate.model';
+import { SetRedirectUrl } from '../store/actions/auth.actions';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +11,17 @@ import { SnackService } from 'src/app/services/snack.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private afAuth: AngularFireAuth,
-    private snack: SnackService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {}
 
-  async canActivate(): Promise<boolean> {
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     const user = await this.afAuth.currentUser;
     const isLoggedIn = !!user;
 
     if (!isLoggedIn) {
+      this.store.dispatch(new SetRedirectUrl(state.url));
       this.router.navigate(['/']);
-      this.snack.authError();
     }
 
     return isLoggedIn;
