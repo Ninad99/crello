f1eb635ebbabe45bf604986e508d71035a385a3e
ninad99 <ninuch7@gmail.com>
2020-06-21 00:46:12 +0530

    Added more functionality

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
-import { Project } from '../models/project.model';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { Project } from '../models/project.model';
+import { Board } from '../models/board.model';
 
 @Injectable({
   providedIn: 'root'
@@ -56,5 +57,69 @@ export class ProjectService {
         }
       })
     );
+  }
+
+  /**
+   * Delete a project
+   */
+  async deleteProject(projectId: string): Promise<boolean> {
+    const user = await this.afAuth.currentUser;
+    const projectRef = this.db.collection('projects').doc(projectId).ref;
+    const projectData = (await projectRef.get()).data() as Project;
+
+    if (projectData.uid === user.uid) {
+      const boards = projectData.boards as string[];
+
+      if (boards?.length > 0) {
+        boards.forEach(async boardId => {
+          await this.deleteBoard(boardId);
+        });
+      }
+
+      await projectRef.delete();
+      return Promise.resolve(true);
+    }
+
+    return Promise.resolve(false);
+  }
+
+  /**
+   * Get all boards of current project
+   */
+  async getProjectBoards(boardIds: string[]): Promise<Board[]> {
+    const boards: Board[] = [];
+
+    for (const boardId of boardIds) {
+      const board = (await this.db.collection<Board>('boards').doc(boardId).ref.get()).data();
+      boards.push(board);
+    }
+
+    return Promise.resolve(boards);
+  }
+
+  /**
+   * Creates a new board for the current user
+   */
+  async createBoard(data: { order: number; title: string; projectId: string }): Promise<void> {
+    const user = await this.afAuth.currentUser;
+
+    const boardRef = await this.db.collection('boards').add({
+      ...data,
+      uid: user.uid,
+      tasks: [{ description: 'Hello!', label: 'Yellow' }]
+    });
+
+    const projectRef = this.db.collection('projects').doc(data.projectId).ref;
+    const oldBoards = (await projectRef.get()).data().boards;
+    const newBoards = [...oldBoards, boardRef.id];
+
+    return this.db.collection('projects').doc(data.projectId).update({ boards: newBoards });
+  }
+
+  /**
+   * Delete board
+   */
+  deleteBoard(boardId: string): Promise<void> {
+    return this.db.collection('boards').doc(boardId).delete();
   }
 }
