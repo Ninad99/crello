92918cc2c5f591562acc1cc8d61be41579c95eaa
ninad99 <ninuch7@gmail.com>
2020-06-27 23:27:57 +0530

    Preloader bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone, AfterViewInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Store } from '@ngrx/store';
 import { AppState } from './models/appstate.model';
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   loading: boolean;
   _redirectSubscription: Subscription;
   redirectUrl: string;
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this.loading = true;
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this._redirectSubscription = this.store
       .select(state => state.auth.redirectUrl)
       .subscribe((url: string) => {
@@ -37,14 +37,19 @@ export class AppComponent implements OnInit, OnDestroy {
       if (user) {
         this.store.dispatch(new LoginSuccess());
         if (this.redirectUrl) this.ngZone.run(() => this.router.navigate([this.redirectUrl]));
-        this.loading = false;
-      } else {
-        this.loading = false;
       }
     });
   }
 
   ngOnDestroy(): void {
     if (this._redirectSubscription) this._redirectSubscription.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.setLoading(false), 1000);
+  }
+
+  setLoading(value: boolean): void {
+    this.loading = value;
   }
 }
